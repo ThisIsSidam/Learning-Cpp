b3519f75eb880c976b53d6c1b34d9768e42d0626
Anshu Kumar Singh <thisissidam@gmail.com>
2023-04-10 17:47:21 +0530

    Add files via upload

---
+++ PracticePrograms/CurrencyConverter/CurrencyConverter.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <exception>
+
+class IllegalCurrency : public std::exception {
+
+public: 
+    virtual const char* what () const noexcept {
+        return "Illegal Currency Entered\n";
+    }
+};
+
+// I don't know how to get real time exchange rates. And I want to just do 
+// things mostly with what I learned on the Udemy Course. 
+std::map<std::string, double> currencies {
+    {"INR", 1}, {"USD", 81.93}, {"EUR", 89.42}, {"GBP", 101.76}, {"KWD", 266.75},
+    {"AED", 22.31}, {"JPY",0.62} 
+};
+
+
+void guidebook();
+double currency_changer(const double&, const std::string &, const std::string &);
+
+int main() {
+
+    const std::string guide {"GUIDE"};
+    const std::string quit {"QUIT"};
+
+    std::cout << "\n== Sidam's Currency Converter Activated ========" << std::endl;
+    
+    bool guide_seen {false};
+
+    while (true) {
+
+        if (!guide_seen)
+        {
+            guidebook();
+            guide_seen = true;
+        }
+
+        std::string primary_currency;
+        std::string secondary_currency;
+        double amount;
+
+        std::string str;
+
+        std::cout << "Enter: ";
+        std::getline(std::cin, str);
+
+        std::stringstream ss {str};
+
+        if (! (ss >> amount) )
+        {
+            if ( ss.str() == guide )
+            {
+                guidebook();
+                continue;
+            }
+            else if ( ss.str() == quit )
+            {
+                std::cout << "Goodbye!" << std::endl;
+                break;
+            }
+            else 
+            {
+                std::cout << ss.str() << std::endl;
+                std::cout << "Illegal Values Entered!" << std::endl;
+                continue;
+            }
+        }
+        else 
+        {
+            if (! (ss >> primary_currency) )
+            {
+                std::cerr << "Illegal Currency Entered";
+            }
+            else 
+            {
+                if (! (ss >> secondary_currency) )
+                {
+                    std::cerr << "Illegal Currency Entered";
+                }
+            }
+        }
+
+        try{
+            double secondary_amount = currency_changer(amount, primary_currency, secondary_currency);
+            std::cout << amount << " " << primary_currency << " = "
+                    << secondary_amount << " " << secondary_currency << std::endl;
+        }
+        catch (const IllegalCurrency &ex)
+        {
+            std::cerr << ex.what();
+        } 
+
+    } //While Loop ends here.
+
+    return 0;
+}
+
+void guidebook() {
+    std::cout << "\n== Guide =====";
+
+    std::cout << "\nEnter Amount, Currency of Amount and the Currency you wish to change to."
+              << "\nAll Separated with spaces."
+              << "\nUse all capital Currency Codes only."
+              << "\nSupported Currencies: INR, USD, EUR, GBP, KWD, AED, JPY." 
+              << "\n\nEnter 'GUIDE' to see the guide again."
+              << "\nEnter 'QUIT' to end the program."
+              << "\nRemember that these commands are all capital.";
+
+    std::cout << "\n==============\n" << std::endl;
+
+}
+
+double currency_changer(const double &amount, const std::string &primary, const std::string &secondary) {
+    
+    double new_amount;
+    auto it = currencies.begin();
+
+    bool part_one_complete {0};
+    while ( it != currencies.end() )
+    {
+        if ( (it->first == primary) and (!part_one_complete) ) 
+        {
+            new_amount = amount * (it->second);
+            part_one_complete = true;
+            it = currencies.begin();
+        }
+
+        if ( (it->first == secondary) and (part_one_complete) )
+        {
+            return new_amount/(it->second);
+        }
+        it++;
+    }
+
+    throw IllegalCurrency();
+}
